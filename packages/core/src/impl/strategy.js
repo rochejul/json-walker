8a8717191fecd87dfeb6f83980b96eae64f7d23d
Julien Roche <roche.jul@gmail.com>
2024-12-08 22:52:40 +0100

    fix: remove circular dependency

--- packages/core/src/impl/strategy.js
+++ packages/core/src/impl/strategy.js
@@ -1,7 +1,11 @@
 import { getPropertyType, WalkerPropertyType } from '@json-walker/util';
-import { ArrayWalker } from './array-walker';
-import { DeadEndWalker } from './dead-end-walker';
-import { ObjectWalker } from './object-walker';
+import { Option } from '@json-walker/util';
+import {
+  ArrayWalkerPathExpression,
+  MemberWalkerPathExpression,
+  WalkerPath,
+} from '../models/walker-path';
+import { WalkerMetadata } from '../models/walker-metadata';
 
 /**
  * @param {{ value: *, currentWalkerPath: WalkerPath }} param
@@ -19,4 +23,126 @@ export function getWalker({ value, currentWalkerPath }) {
   }
 
   return new DeadEndWalker({ value, currentWalkerPath });
+}
+
+class ArrayWalker {
+  #array;
+  #currentWalkerPath;
+  #index = 0;
+  #walker;
+  #lastOptionalWalkerMetadata;
+
+  constructor({ value, currentWalkerPath }) {
+    this.#array = value;
+    this.#currentWalkerPath = currentWalkerPath;
+
+    if (!currentWalkerPath.isRootLevel()) {
+      this.#walker = new DeadEndWalker({ value, currentWalkerPath });
+    }
+  }
+
+  nextStep() {
+    if (this.#walker) {
+      this.#lastOptionalWalkerMetadata = this.#walker.nextStep();
+
+      if (this.#lastOptionalWalkerMetadata.isSome()) {
+        return this.#lastOptionalWalkerMetadata;
+      }
+
+      this.#walker = null;
+    }
+
+    if (this.#index < this.#array.length) {
+      const currentIndex = this.#index;
+      const value = this.#array[this.#index];
+      this.#index += 1;
+
+      const { paths } = this.#currentWalkerPath;
+      const walkerPath = new WalkerPath({
+        paths: [
+          ...paths,
+          new ArrayWalkerPathExpression({ expression: currentIndex }),
+        ],
+      });
+
+      this.#walker = getWalker({ value, currentWalkerPath: walkerPath });
+
+      this.#lastOptionalWalkerMetadata = this.#walker.nextStep();
+      return this.#lastOptionalWalkerMetadata;
+    }
+
+    return Option.none();
+  }
+}
+
+class DeadEndWalker {
+  #lastOptionalWalkerMetadata;
+
+  constructor({ value, currentWalkerPath }) {
+    this.#lastOptionalWalkerMetadata = Option.from(
+      new WalkerMetadata({
+        propertyName: '',
+        propertyValue: value,
+        propertyPath: currentWalkerPath,
+      }),
+    );
+  }
+
+  nextStep() {
+    const optionalWalkerMetadata = this.#lastOptionalWalkerMetadata;
+    this.#lastOptionalWalkerMetadata = Option.none();
+    return optionalWalkerMetadata;
+  }
+}
+
+class ObjectWalker {
+  #array;
+  #object;
+  #currentWalkerPath;
+  #index = 0;
+  #walker;
+  #lastOptionalWalkerMetadata;
+
+  constructor({ value, currentWalkerPath }) {
+    this.#object = value;
+    this.#array = Object.getOwnPropertyNames(value);
+    this.#currentWalkerPath = currentWalkerPath;
+
+    if (!currentWalkerPath.isRootLevel()) {
+      this.#walker = new DeadEndWalker({ value, currentWalkerPath });
+    }
+  }
+
+  nextStep() {
+    if (this.#walker) {
+      this.#lastOptionalWalkerMetadata = this.#walker.nextStep();
+
+      if (this.#lastOptionalWalkerMetadata.isSome()) {
+        return this.#lastOptionalWalkerMetadata;
+      }
+
+      this.#walker = null;
+    }
+
+    if (this.#index < this.#array.length) {
+      const propertyName = this.#array[this.#index];
+      const value = this.#object[propertyName];
+      this.#index += 1;
+
+      const { paths } = this.#currentWalkerPath;
+      const walkerPath = new WalkerPath({
+        paths: [
+          ...paths,
+          new MemberWalkerPathExpression({ expression: propertyName }),
+        ],
+      });
+
+      this.#walker = getWalker({ value, currentWalkerPath: walkerPath });
+
+      this.#lastOptionalWalkerMetadata = this.#walker.nextStep();
+      return this.#lastOptionalWalkerMetadata;
+    }
+
+    return Option.from(null);
+  }
 }
